fb0d5d638e0271c4ee73a0dd92b3e486f19611b1
AnthonyLamour <a.lamour@ludus-academie.com>
2020-11-03 15:53:35 +0100

    Ajout Singleton

---
+++ UnityPattern/Assets/_Scripts/CubeSpwaner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpwaner : MonoBehaviour
+{
+
+    public GameObject cubeObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(cubeObj, this.transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
